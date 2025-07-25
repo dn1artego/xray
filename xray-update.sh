041628fe563e1407e6f478364905128c4a63c8c4
dn.artego <dn.artego@gmail.com>
2025-07-25 22:56:59 +0300

    update

--- xray-update.sh
+++ xray-update.sh
@@ -1,18 +1,1 @@
 #!/bin/bash
-
-# 1. Проверка на root-права
-if [[ $(id -u) -ne 0 ]]; then
-   echo "Этот скрипт нужно запускать с правами root (используйте sudo)."
-   exit 1
-fi
-
-echo "Запускаем обновление Xray-core..."
-bash -c "$(curl -4 -L https://github.com/XTLS/Xray-install/raw/main/install-release.sh)" @ install
-
-echo "Обновляем файлы GeoIP и GeoSite..."
-bash -c "$(curl -4 -L https://github.com/XTLS/Xray-install/raw/main/install-release.sh)" @ install-dat-release
-
-echo "Перезапускаем службу Xray..."
-systemctl restart xray
-
-echo "Обновление успешно завершено!"
